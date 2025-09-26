08d9f7b9731d782d46231960dce9d5eea9d5f226
CharSui <798296184@qq.com>
2025-09-26 15:09:49 +0800

    Feature : 补充Event的简单实现，一些兼容2019的API调整

--- Runtime/Logger/CharLogger.cs
+++ Runtime/Logger/CharLogger.cs
@@ -3,10 +3,7 @@
  * Created On: 2025.09.16
  * Description: 日志记录器，提供日志记录功能
  */
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Diagnostics;
+
 using System.Runtime.CompilerServices;
 
 public class CharLogger
@@ -16,7 +13,7 @@ public class CharLogger
         [CallerFilePath] string filePath = "")
     {
         string className = System.IO.Path.GetFileNameWithoutExtension(filePath);
-        Debug.Log($"[{className}.{methodName}] {message}");
+        UnityEngine.Debug.Log($"[{className}.{methodName}] {message}");
     }
 
     public static void LogError(object message,
@@ -24,6 +21,6 @@ public class CharLogger
         [CallerFilePath] string filePath = "")
     {
         string className = System.IO.Path.GetFileNameWithoutExtension(filePath);
-        Debug.LogError($"[{className}.{methodName}] {message}");
+        UnityEngine.Debug.LogError($"[{className}.{methodName}] {message}");
     }
 }
